1464c9330d88213df60e3576817e6a9720e2695e
Krishna Pandit <jivkrishna@outlook.com>
2025-02-21 13:32:11 +0000

    test for string '1, 5'

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -11,5 +11,9 @@ RSpec.describe StringCalculator do
     it "returns 1 for '1'" do
       expect(calculator.add("1")).to eq(1)
     end
+
+    it "returns 3 for '1,5'" do
+      expect(calculator.add("1,5")).to eq(6)
+    end
   end
 end
