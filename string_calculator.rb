feaae8fdd4745bc072555814c83e9a16ac1b98bf
Krishna Pandit <jivkrishna@outlook.com>
2025-02-21 14:43:37 +0000

    code for negative numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -11,6 +11,13 @@ class StringCalculator
       input = parts[1]
     end
 
-    input.split(delimiter).map(&:to_i).sum
+    # Split input string by delimiter and convert to integers
+    numbers = input.split(delimiter).map(&:to_i)
+
+    # Negative numbers not allowed
+    negative_numbers = numbers.select { |n| n < 0 }
+    raise ArgumentError, "negative numbers not allowed: #{negative_numbers.join(", ")}" if negative_numbers.any?
+
+    numbers.sum
   end
 end
