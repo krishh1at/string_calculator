0be6bf259f6e5602bc0c988630386e462d55af30
Krishna Pandit <jivkrishna@outlook.com>
2025-02-21 13:50:22 +0000

    context wise test refactor

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,28 +4,36 @@ RSpec.describe StringCalculator do
   let(:calculator) { StringCalculator.new }
 
   describe ".add" do
-    it "returns 0 for empty string" do
-      expect(calculator.add("")).to eq(0)
+    context "for empty string" do
+      it "returns 0" do
+        expect(calculator.add("")).to eq(0)
+      end
     end
 
-    it "returns 1 for '1'" do
-      expect(calculator.add("1")).to eq(1)
+    context "for single number" do
+      it "returns 1 for '1'" do
+        expect(calculator.add("1")).to eq(1)
+      end
     end
 
-    it "returns 3 for '1,5'" do
-      expect(calculator.add("1,5")).to eq(6)
+    context "for comma separated numbers" do
+      it "returns 3 for '1,5'" do
+        expect(calculator.add("1,5")).to eq(6)
+      end
+  
+      it "returns 6 for '1,2,3'" do
+        expect(calculator.add("1,2,3")).to eq(6)
+      end
+  
+      it "returns 6 for '1,2,3,4'" do
+        expect(calculator.add("1,2,3,4")).to eq(10)
+      end
     end
 
-    it "returns 6 for '1,2,3'" do
-      expect(calculator.add("1,2,3")).to eq(6)
-    end
-
-    it "returns 6 for '1,2,3,4'" do
-      expect(calculator.add("1,2,3,4")).to eq(10)
-    end
-
-    it "returns 6 for '1\n2,3'" do
-      expect(calculator.add("1\n2,3")).to eq(6)
+    context "for newline separated numbers" do
+      it "returns 6 for '1\n2,3'" do
+        expect(calculator.add("1\n2,3")).to eq(6)
+      end
     end
   end
 end
