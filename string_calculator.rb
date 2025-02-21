5a4248fd6ea3acc7c95c3035a1f470cca399e037
Krishna Pandit <jivkrishna@outlook.com>
2025-02-21 14:27:31 +0000

    code for custom delimiter

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,6 +2,15 @@ class StringCalculator
   def add(input)
     0 if input.empty?
 
-    input.split(/,|\n/).map(&:to_i).sum
+    delimiter = /,|\n/  # Default delimiters: comma `,` or newline `\n`
+
+    # Custom delimiter
+    if input.start_with?("//")
+      parts = input.split("\n", 2)
+      delimiter = parts[0][2]
+      input = parts[1]
+    end
+
+    input.split(delimiter).map(&:to_i).sum
   end
 end
