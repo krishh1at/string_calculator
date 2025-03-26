d65a841b85e7daa00635a1430ae7ad2fbfe470df
Krishna Pandit <jivkrishna@outlook.com>
2025-03-26 05:43:02 +0000

    refactor

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,23 +1,39 @@
 class StringCalculator
-  def add(input)
-    return 0 if input.empty?
+  DEFAULT_DELIMITER = /,|\n/.freeze # Default delimiters: comma `,` or newline `\n`
 
-    delimiter = /,|\n/ # Default delimiters: comma `,` or newline `\n`
+  def add(str)
+    return 0 if str.empty?
 
-    # Custom delimiter
-    if input.start_with?("//")
-      parts = input.split("\n", 2)
-      delimiter = parts[0][2]
-      input = parts[1]
-    end
+    custom_delimiter, input = extract_inputs(str)
+    delimiter = custom_delimiter || DEFAULT_DELIMITER
 
-    # Split input string by delimiter and convert to integers
-    numbers = input.split(delimiter).map(&:to_i)
-
-    # Negative numbers not allowed
-    negative_numbers = numbers.select { |n| n < 0 }
-    raise ArgumentError, "negative numbers not allowed: #{negative_numbers.join(", ")}" if negative_numbers.any?
+    numbers = parse_numbers(input, delimiter)
+    validate_numbers(numbers)
 
     numbers.sum
+  end
+
+  private
+
+  # extract delimiter & input numbers
+  def extract_inputs(str)
+    return [nil, str] unless str.start_with?("//")
+
+    parts = str.split("\n", 2)
+    delimiter = parts[0][2]
+    input = parts[1]
+
+    [delimiter, input]
+  end
+
+  # Parse numbers from the input string using the given delimiter
+  def parse_numbers(input, delimiter)
+    input.split(delimiter).map(&:to_i)
+  end
+
+  # Validate numbers (e.g., check for negatives)
+  def validate_numbers(numbers)
+    negative_numbers = numbers.select { |n| n < 0 }
+    raise ArgumentError, "negative numbers not allowed: #{negative_numbers.join(', ')}" if negative_numbers.any?
   end
 end
